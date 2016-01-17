9d52c5b61330ddfbaab0750a9ff4740d6f6908c4
dagoeba <xamab@mail.bg>
2016-01-17 22:36:00 +0200

    Remove toolbar and settings

--- app/src/main/java/com/xamab/geoquiz/QuizActivity.java
+++ app/src/main/java/com/xamab/geoquiz/QuizActivity.java
@@ -46,20 +46,22 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
-        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
-        int question = mQuestionBank[mCurrentIndex].getTextResId();
-        mQuestionTextView.setText(question);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mNextButton = (Button) findViewById((R.id.next_button));
+        mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex+1) % mQuestionBank.length;
+                updateQuestion();
             }
         });
+
+        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+        updateQuestion();
+    }
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
     }
 
     @Override
